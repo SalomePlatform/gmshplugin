a1560a00309632a8863a2f11ae25325223d53425
Paul RASCLE <paul.rascle@edf.fr>
2016-01-15 09:20:37 +0100

    portage V7_7_0

--- src/GUI/GMSHPluginGUI_HypothesisCreator.cxx
+++ src/GUI/GMSHPluginGUI_HypothesisCreator.cxx
@@ -30,7 +30,7 @@
 
 #include <SalomeApp_Tools.h>
 #include <LightApp_SelectionMgr.h>
-#include <SALOME_ListIteratorOfListIO.hxx>
+#include <SALOME_ListIO.hxx>
 
 #include <QComboBox>
 #include <QLabel>

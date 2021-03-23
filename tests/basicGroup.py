ea29bad5258e549d1e8b99d185b17075b4a5d8c4
vsr <vsr@opencascade.com>
2021-03-23 19:37:08 +0300

    Copyright update 2021

--- tests/basicGroup.py
+++ tests/basicGroup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2012-2015  ALNEOS
-# Copyright (C) 2016-2020  EDF R&D
+# Copyright (C) 2016-2021  EDF R&D
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public

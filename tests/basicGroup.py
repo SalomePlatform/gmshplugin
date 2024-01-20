4f01cc056b525e8a0adb07ad280687ddf68bd62c
mbs <martin.bernhard@opencascade.com>
2024-01-20 13:53:38 +0000

    Updated copyright comment

--- tests/basicGroup.py
+++ tests/basicGroup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2012-2015  ALNEOS
-# Copyright (C) 2016-2023  EDF
+# Copyright (C) 2016-2024  EDF
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
